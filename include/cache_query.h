41c25051ee3c2fb27894e7ae27ca87a4e4dbc2ea
Érico Nogueira <erico.erc@gmail.com>
2021-07-31 00:40:53 -0300

    Comment cache_add and cache_query headers.

--- include/cache_query.h
+++ include/cache_query.h
@@ -1,3 +1,18 @@
+/* This code is written this way in order to implement polimorphism: the struct
+ * passwd and struct group caches are implemented in exactly the same way, the
+ * only difference being the name of struct members and struct layout.
+ *
+ * The function to query values from cache will #include this file in its body,
+ * after defining the necessary macros:
+ *   - CACHE: name of the cache for that struct
+ *   - RESULT_TYPE: the type of a result stored in the cache
+ *   - HASH_ARG: the argument that will be passed to the hashing function, if
+ *     any
+ *   - COMPARISON: the equality check to determine if the entry being queried is present in cache already
+ *   - ARGUMENT: the name of the struct member holding the struct passwd or
+ *     struct group being used
+ */
+
 IS_CACHING
 
 enum nss_status ret = NSS_STATUS_NOTFOUND;
